858423970aac5a4b610dab0d9454d50b62986feb
Dylan Bailey <wearesleepless@gmail.com>
2023-09-15 14:42:42 -0400

    Firebase connection

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-19ef3-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputEl = document.getElementById("input-field")
 const addBtn = document.getElementById("add-button")
 
 addBtn.addEventListener("click", function() {
     let inputValue = inputEl.value
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
 })
