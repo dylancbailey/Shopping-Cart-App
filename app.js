b699b6f10b32ea2b5c240eb66ce4f2d81f193d1f
Dylan Bailey <wearesleepless@gmail.com>
2023-09-15 13:46:06 -0400

    Basic functionality

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputEl = document.getElementById("input-field")
+const addBtn = document.getElementById("add-button")
+
+addBtn.addEventListener("click", function() {
+    let inputValue = inputEl.value
+    console.log(inputValue)
+})
